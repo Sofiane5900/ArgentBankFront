c3d5e2e313e525af0064f0f636221bacb0a4763c
Sofiane5900 <iguedjetalsofiane@gmail.com>
2024-04-19 17:56:53 +0200

    intégration page profile

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes  } from 'react-router-dom';
 import './App.css';
 import Home from './Pages/Home';
 import SignIn from './Pages/SignIn';
+import User from './Pages/User';
 
 
 function App() {
@@ -12,6 +13,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/profile" element={<User />} />
+        <Route path="*" element={<div>Error 404</div>} />
+
       </Routes>
       </Router>
 
---
+++ src/Pages/User.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../components/Navbar/Navbar'
+import Account from '../components/Account/Account';
+import Footer from '../components/Footer/Footer';
+
+const User = () => {
+  return (
+    <div>
+        <Navbar />
+        <Account />
+        <Footer />
+    </div>
+  )
+}
+
+export default User
